9f6e4f7d6c69fb697d9a29dadcb418eb46857015
sameerdash2 <dash.sameer@gmail.com>
2020-05-11 18:08:20 -0700

    Remove graph (will stay on other branches)

--- src/public/script.js
+++ src/public/script.js
@@ -18,32 +18,15 @@ document.addEventListener("DOMContentLoaded", function() {
     let message = document.getElementById("message");
     let commentsSection = document.getElementById("commentsSection");
     let loadStatus = document.getElementById("loadStatus");
-    let viewGraph = document.getElementById("viewGraph");
-    let startDate = document.getElementById("startDate");
-    let endDate = document.getElementById("endDate");
     let info = document.getElementById("info");
     let showMoreBtn = document.getElementById("showMoreBtn");
     let linkedHolder = document.getElementById("linkedHolder");
-    let chartContainer = document.getElementById("chartContainer");
 
     let storedReplies = {};
 
     submitBtn.disabled = false;
     submitBtn.innerHTML = "Submit";
     showMoreBtn.innerHTML = `Show ` + MAXDISPLAY + ` more comments...`;
-
-    // Graph setup
-    let ctx = document.getElementById('graph');
-    Chart.defaults.global.defaultFontColor = '#222222';
-    Chart.defaults.global.defaultFontSize = 14;
-    let timeChart;
-    let gridLines = {
-        color: 'rgba(0, 0, 0, 0.2)',
-        drawTicks: false
-    }
-    let graphShown = false;
-    let startText, endText;
-    let graphPoints = [];
 
     document.getElementById("videoForm").addEventListener('submit', function(event){
         event.preventDefault(); // prevents page reloading
@@ -77,59 +60,6 @@ document.addEventListener("DOMContentLoaded", function() {
             socket.emit("sortRequest", closest.id.substring(2));
         }
     });
-    viewGraph.addEventListener('click', function() {
-        if (graphShown) {
-            chartContainer.style.display = "none";
-            graphShown = false;
-        }
-        else if (graphPoints.length > 0) {
-            chartContainer.style.display = "block";
-            graphShown = true;
-        }
-        else {
-            viewGraph.disabled = true;
-            socket.emit("graphRequest");
-        }
-
-    });
-    
-    let inputs = document.querySelectorAll(".enterBounds");
-    inputs.forEach(function(elem) {
-        elem.addEventListener('keydown', boundUpdate);
-    });
-    function boundUpdate(event) {        
-        if (event.keyCode == 13) {
-            updateGraphBounds();
-        }
-    }
-
-    document.getElementById("resetBounds").addEventListener('click', function() {
-        startDate.value = startText;
-        endDate.value = endText;
-        updateGraphBounds();
-    });
-
-    function updateGraphBounds() {
-        let newStart = new Date(startDate.value);
-        let newEnd = new Date(endDate.value);
-        if (!isNaN(newStart)) timeChart.options.scales.xAxes[0].ticks.min = newStart.toISOString().substring(0, 10);
-        if (!isNaN(newEnd)) timeChart.options.scales.xAxes[0].ticks.max = newEnd.toISOString().substring(0, 10);
-        
-        timeChart.options.scales.yAxes[0].ticks.max = calcGraphMax();
-        
-        timeChart.update();
-    }
-
-    function calcGraphMax(start = timeChart.options.scales.xAxes[0].ticks.min, end = timeChart.options.scales.xAxes[0].ticks.max) {
-        let m = 1;
-        let len = graphPoints.length;
-        for (let i = 0; i < len; i++) {
-            if (graphPoints[i].x >= start && graphPoints[i].x <= end) {
-                m = Math.max(m, graphPoints[i].y);
-            }
-        }
-        return Math.ceil(m * 1.1);
-    }
 
     commentsSection.addEventListener('click', repliesButton);
     linkedHolder.addEventListener('click', repliesButton);
@@ -173,7 +103,6 @@ document.addEventListener("DOMContentLoaded", function() {
     socket.on("commentsInfo", ({num, disabled, eta, commence}) => {
         let commentInfo = document.getElementById("commentInfo");
         document.getElementById("chooseLoad").style.display = (!disabled && !commence && num < MAX) ? "block" : "none";
-        viewGraph.style.display = num >= 50 ? "block" : "none";
         if (disabled) {
             commentInfo.innerHTML = `<i class="fas fa-comment"></i> Comments have been disabled on this video.`;
             if (num > 0) {
@@ -242,91 +171,6 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
-    socket.on("graphData", function({data, published}) {
-        if (graphShown) {
-            timeChart.destroy();
-        }
-        graphPoints = data;
-        chartContainer.style.display = "block";
-
-        // Declare left bound as video publish date, unless there's a comment before publish
-        let start = new Date(Math.min(new Date(published), new Date(data[0].x)));
-        //start.setDate(start.getDate() - 1);
-        let end = new Date();
-        startText = start.toISOString().substring(0, 10);
-        endText = end.toISOString().substring(0, 10);
-        startDate.value = startText;
-        endDate.value = endText;
-
-        let yMax = calcGraphMax(startText, endText);
-
-        timeChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: ['Red', 'Blue'],
-                datasets: [{
-                    label: 'Comments',
-                    data: graphPoints,
-                    backgroundColor: 'rgba(0,0,100,1)',
-                    fill: false,
-                    // pointRadius: 10,
-                    pointHitRadius: 30,
-                    pointHoverRadius: 10,
-                    pointBackgroundColor: 'rgba(0,0,0,0)',
-                    pointBorderColor: 'rgba(0,0,0,0)',
-                    pointBorderWidth: 0,
-                    lineTension: 0.0,
-                    borderWidth: 3,
-                    borderColor: 'rgb(0,0,0)'
-                }]
-            },
-            options: {
-                maintainAspectRatio: false,
-                responsive:false,
-                // hover: { animationDuration: 0 },
-                layout: { padding: {right: 25} },
-                scales: {
-                    xAxes: [{
-                        type: 'time',
-                        gridLines: gridLines,
-                        offset: false,
-                        time: {
-                            tooltipFormat: 'YYYY-MM-DD',
-                            parser: 'YYYY-MM-DD',
-                            // unit: 'day',
-                            minUnit: 'day',
-                            // stepSize: 1,
-                            displayFormats: {
-                                month: 'MMM YYYY',
-                                day: 'M/D/YYYY'
-                            }
-                        },
-                        ticks: {
-                            sampleSize: 1,
-                            // autoSkip: false,
-                            // autoSkipPadding: 11,
-                            padding: 10,
-                            maxRotation: 0,
-                            min: startText,
-                            max: endText
-                        }
-                    }],
-                    yAxes: [{
-                        gridLines: gridLines,
-                        ticks: {
-                            max: yMax,
-                            padding: 5,
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-        graphShown = true;
-        viewGraph.disabled = false;
-        
-    });
-
     socket.on("resetPage", resetPage);
     function resetPage() {
         linkedHolder.innerHTML = "";
@@ -340,21 +184,9 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById("b_likesMost").disabled = false;
         document.getElementById("b_dateNewest").disabled = false;
         document.getElementById("b_dateOldest").disabled = true;
-        viewGraph.disabled = false;
-        viewGraph.style.display = "none"; //safety
         session = {
             commentNum: 0,
         };
-
-        startText = "";
-        endText = "";
-
-        if (graphPoints.length > 0) {
-            timeChart.destroy();
-            graphShown = false;
-            chartContainer.style.display = "none";
-            graphPoints = [];
-        }
         
         storedReplies = {};
     }
