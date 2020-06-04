00553ec08b44b613e2550f9f5385806c7c1992ab
sameerdash2 <dash.sameer@gmail.com>
2020-06-03 17:53:47 -0700

    Fix bug that puts all users on one instance

--- index.js
+++ index.js
@@ -18,71 +18,71 @@ class App {
     createServer() {
         io.on('connection', (socket) => {
             console.log('a user connected');
-            this._video = new Video(this, socket);
+            let videoInstance = new Video(this, socket);
             
             socket.on('disconnect', () => {
                 console.log('user disconnected');
             });
             socket.on('idSent', (id) => {
-                if (!this.checkSendID(id)) socket.emit("idInvalid");
+                if (!checkSendID(id)) socket.emit("idInvalid");
             });
             socket.on("requestAll", () => {
-                this._video.handleLoad("dateOldest");
+                videoInstance.handleLoad("dateOldest");
             })
             socket.on("showMore", () => {
-                this._video.sendLoadedComments();
+                videoInstance.sendLoadedComments();
             });
             socket.on("sortRequest", (type) => {
-                this._video.doSort(type);
+                videoInstance.doSort(type);
             });
             socket.on("replyRequest", (id) => {
-                this._video.getReplies(id);
+                videoInstance.getReplies(id);
             });
             socket.on("graphRequest", () => {
-                this._video.makeGraph();
+                videoInstance.makeGraph();
             });
-        });
-    }
     
-    checkSendID(inp) {
-        // Assuming video ID length of 11
-        if (inp.length >= 11) {
-            let marker = inp.indexOf("v=");
-            let idString = "";
-            if (marker > -1 && inp.length >= marker + 2 + 11) {
-                // Normal "watch?v=" link
-                idString = inp.substring(marker + 2, marker + 2 + 11);
-            }
-            else {
-                // youtu.be or ID only
-                idString = inp.substring(inp.length - 11);
-            }
-            
-            let linked = inp.indexOf("lc=");
-            if (linked > -1) {
-                let linkedId = inp.substring(linked + 3);
-                let linkedParentId;
-                if (linkedId.indexOf(".") > -1) {
-                    // Linked a reply
-                    let dot = linkedId.indexOf(".");
-                    linkedParentId = linkedId.substring(0, dot);
-                    this._video.fetchLinkedComment(idString, linkedParentId, linkedId);
+            function checkSendID(inp) {
+                // Assuming video ID length of 11
+                if (inp.length >= 11) {
+                    let marker = inp.indexOf("v=");
+                    let idString = "";
+                    if (marker > -1 && inp.length >= marker + 2 + 11) {
+                        // Normal "watch?v=" link
+                        idString = inp.substring(marker + 2, marker + 2 + 11);
+                    }
+                    else {
+                        // youtu.be or ID only
+                        idString = inp.substring(inp.length - 11);
+                    }
+                    
+                    let linked = inp.indexOf("lc=");
+                    if (linked > -1) {
+                        let linkedId = inp.substring(linked + 3);
+                        let linkedParentId;
+                        if (linkedId.indexOf(".") > -1) {
+                            // Linked a reply
+                            let dot = linkedId.indexOf(".");
+                            linkedParentId = linkedId.substring(0, dot);
+                            videoInstance.fetchLinkedComment(idString, linkedParentId, linkedId);
+                        }
+                        else {
+                            // Linked a parent comment
+                            linkedParentId = linkedId;
+                            videoInstance.fetchLinkedComment(idString, linkedParentId);
+                        }
+                    }
+                    else {
+                        videoInstance.fetchTitle(idString, false);
+                    }
+                    
+                    return true;
                 }
                 else {
-                    // Linked a parent comment
-                    linkedParentId = linkedId;
-                    this._video.fetchLinkedComment(idString, linkedParentId);
+                    return false;
                 }
             }
-            else {
-                this._video.fetchTitle(idString, false);
-            }
-            
-            return true;
-        }
-        else {
-            return false;
-        }
+        });
     }
 
     listen() {
