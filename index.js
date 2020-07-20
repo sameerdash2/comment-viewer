c716ecdd3f736dd057838a9f00787fa4645e1c7f
sameerdash2 <dash.sameer@gmail.com>
2020-07-19 23:31:42 -0700

    Improve input parsing

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ class App {
     createServer() {
         io.on('connection', (socket) => {
             const videoInstance = new Video(this, io, socket);
-            
-            socket.on('idSent', (inputId) => {
+
+            socket.on("idSent", (inputId) => {
                 if (!checkSendID(inputId.substring(0, 255))) socket.emit("idInvalid");
             });
             socket.on("requestAll", () => {
                 videoInstance.handleLoad("dateOldest");
-            })
-            socket.on("showMore", ({sort, commentNum}) => {
+            });
+            socket.on("showMore", ({ sort, commentNum }) => {
                 videoInstance.sendLoadedComments(sort, commentNum, false);
             });
             socket.on("replyRequest", (id) => {
@@ -35,44 +35,39 @@ class App {
             socket.on("graphRequest", () => {
                 videoInstance.getStatistics();
             });
-    
+
             function checkSendID(inp) {
                 // Assuming video ID length of 11
                 if (inp.length >= 11) {
-                    const marker = inp.indexOf("v=");
+                    const linkedMarker = inp.indexOf("lc=");
+                    const videoMarker = Math.max(inp.indexOf("v=") + 2, inp.indexOf("youtu.be/") + 9);
                     let idString = "";
-                    if (marker > -1 && inp.length >= marker + 2 + 11) {
-                        // Normal "watch?v=" link
-                        idString = inp.substring(marker + 2, marker + 2 + 11);
-                    }
-                    else {
-                        // youtu.be or ID only
-                        idString = inp.substring(inp.length - 11);
-                    }
-                    
-                    const linked = inp.indexOf("lc=");
-                    if (linked > -1) {
-                        const linkedId = inp.substring(linked + 3);
+
+                    if (linkedMarker > -1) {
+                        const linkedId = inp.substring(linkedMarker + 3);
                         let linkedParentId;
                         if (linkedId.indexOf(".") > -1) {
                             // Linked a reply
                             const dot = linkedId.indexOf(".");
                             linkedParentId = linkedId.substring(0, dot);
                             videoInstance.fetchLinkedComment(idString, linkedParentId, linkedId);
-                        }
-                        else {
+                        } else {
                             // Linked a parent comment
                             linkedParentId = linkedId;
                             videoInstance.fetchLinkedComment(idString, linkedParentId);
                         }
-                    }
-                    else {
+                    } else if (inp.length >= videoMarker + 11) {
+                        // https://www.youtube.com/watch?v=dQw4w9WgXcQ, https://youtu.be/dQw4w9WgXcQ
+                        idString = inp.substring(videoMarker, videoMarker + 11);
+                        videoInstance.fetchTitle(idString, false);
+                    } else {
+                        // dQw4w9WgXcQ (assume)
+                        idString = inp.substring(inp.length - 11);
                         videoInstance.fetchTitle(idString, false);
                     }
-                    
+
                     return true;
-                }
-                else {
+                } else {
                     return false;
                 }
             }
@@ -80,15 +75,11 @@ class App {
     }
 
     listen() {
-        let port = process.env.PORT;
-        if (port == null || port == "") {
-            port = 8000;
-        }
+        const port = process.env.PORT || 8000;
         http.listen(port, () => {
             logger.log('info', 'Listening on %s', port);
         });
     }
-
 }
 
 // eslint-disable-next-line no-unused-vars
